bbcf81ebebf7bffdc3b39bcda53600e5e8fb5456
Hizkias Abraham <hizkiasnew@gmail.com>
2024-02-02 07:35:40 +0300

    header layout

---
+++ components/client/header.tsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <div className="invisible md:visible border-x-0 border-inactive border-b-2">
+      <div className="m-4 flex">
+        <div className="flex flex-auto justify-start gap-4">
+          <div>Dashboard</div>
+          <div>Projects</div>
+        </div>
+        <div className="flex flex-auto justify-center">Meadow</div>
+        <div className="flex flex-auto justify-end gap-4">
+          <div>Notifications</div>
+          <div>Profile</div>
+        </div>
+      </div>
+    </div>
+  );
+}
