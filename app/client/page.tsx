68b5ec3e0344c337e5292cbb737ddd92c98babf4
Hizkias Abraham <hizkiasnew@gmail.com>
2024-02-02 16:21:36 +0300

    demo page

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,3 +1,23 @@
+import { Card } from '@/components/shared/card/card';
+import { DatePicker } from '@/components/shared/date-picker';
+import { Icon } from '@/components/shared/icons';
+
 export default function Client() {
-  return <></>;
+  return (
+    <div>
+      <Card title="Projects">
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque
+            magni cupiditate provident dicta mollitia cum saepe eius, ratione
+            accusantium quam labore sed! Laborum porro neque similique ratione
+            accusantium eveniet saepe!
+          </p>
+        </>
+        <div>
+          <DatePicker />
+        </div>
+      </Card>
+    </div>
+  );
 }
