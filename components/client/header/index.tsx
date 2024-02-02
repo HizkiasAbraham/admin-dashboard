5c586b68b74d980db99d80abe7888a99f614487c
Hizkias Abraham <hizkiasnew@gmail.com>
2024-02-02 13:43:49 +0300

    make header responsive mobile

--- components/client/header/index.tsx
+++ components/client/header/index.tsx
@@ -3,16 +3,16 @@ import { HeaderNavItem } from './header-nav-item';
 
 export default function Header() {
   return (
-    <div className="invisible md:visible border-x-0 border-light-grey border-b-2">
-      <div className="m-4 flex">
-        <div className="flex flex-auto justify-start gap-4">
+    <div className="border-x-0 border-light-grey border-b-2">
+      <div className="m-5 flex">
+        <div className="flex flex-auto justify-start gap-4 invisible md:visible">
           <HeaderNavItem icon="Home" isActive={false} text="Dashboard" />
           <HeaderNavItem icon="Location" isActive={true} text={'Projects'} />
         </div>
         <div className="flex flex-auto justify-center">
           <Image alt="" width={120} height={20} src={'/images/logo.png'} />
         </div>
-        <div className="flex flex-auto justify-end gap-4">
+        <div className="flex flex-auto justify-end gap-4 invisible md:visible">
           <HeaderNavItem
             icon="Notification"
             text="Notifications"
