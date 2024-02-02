cc52b79268d45509979bd6cfed76310e8373c089
Hizkias Abraham <hizkiasnew@gmail.com>
2024-02-02 12:51:21 +0300

    add breadcrumb component

---
+++ components/shared/breadcrumb/index.tsx
@@ -0,0 +1,27 @@
+import { Icon } from '../icons';
+import { BreadcrumbInput } from './types';
+export function BreadCrumb(props: BreadcrumbInput) {
+  const { paths } = props;
+  const lastItem = paths?.pop();
+  return (
+    <div className="flex text-xl font-bold gap-2">
+      <p>Dashboard</p>
+      {paths?.length ? (
+        paths.map((pathName) => (
+          <>
+            <Icon.ArrowRight className="h-5 w-5 text-green font-bold text-xl mt-1" />
+            <p>{pathName}</p>
+          </>
+        ))
+      ) : (
+        <></>
+      )}
+      {lastItem && (
+        <>
+          <Icon.ArrowRight className="h-5 w-5 text-green font-bold mt-1" />
+          <p className="text-grey">{lastItem}</p>
+        </>
+      )}
+    </div>
+  );
+}
