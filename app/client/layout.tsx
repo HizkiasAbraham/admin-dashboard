89875308877fed5941ee5e8e03dd4f89b82016e3
Hizkias Abraham <hizkiasnew@gmail.com>
2024-02-02 12:51:52 +0300

    ajust breadcumb on layut

--- app/client/layout.tsx
+++ app/client/layout.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/client/header';
+import { BreadCrumb } from '@/components/shared/breadcrumb';
 
 export default function ClientLayout({
   children,
@@ -8,7 +9,15 @@ export default function ClientLayout({
   return (
     <>
       <Header />
-      {children}
+      <div className="m-5">
+        <div className="flex pb-2">
+          <div className="flex-auto flex justify-start">
+            <BreadCrumb paths={['Portifolio 1']} />
+          </div>
+          <div className="flex-auto flex justify-end">End</div>
+        </div>
+        {children}
+      </div>
     </>
   );
 }
--- components/shared/icons.jsx
+++ components/shared/icons.jsx
@@ -2,10 +2,12 @@ import HomeIcon from '@heroicons/react/24/outline/HomeIcon';
 import LocationIcon from '@heroicons/react/24/outline/MapPinIcon';
 import NotificationIcon from '@heroicons/react/24/outline/BellIcon';
 import UserAcconutIcon from '@heroicons/react/24/outline/UserIcon';
+import ArrowRightIcon from '@heroicons/react/24/outline/ArrowRightIcon';
 
 export const Icon = {
   Home: (props) => <HomeIcon {...props} />,
   Location: (props) => <LocationIcon {...props} />,
   Notification: (props) => <NotificationIcon {...props} />,
   Account: (props) => <UserAcconutIcon {...props} />,
+  ArrowRight: (props) => <ArrowRightIcon {...props} />,
 };
