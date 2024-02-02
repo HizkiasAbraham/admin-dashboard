2ff091b9a831f3f233794c5599d6200c45d03d7d
Hizkias Abraham <hizkiasnew@gmail.com>
2024-02-02 09:53:40 +0300

    add header nav items component

---
+++ components/client/header-nav-item.tsx
@@ -0,0 +1,16 @@
+import { NavItemProp } from '@/components/client/types';
+import { Icon } from '@/components/shared/icons';
+
+export function HeaderNavItem(prop: NavItemProp) {
+  const { icon, text, isActive } = prop;
+
+  const NavIcon = Icon[icon];
+  return (
+    <div className="flex gap-1 cursor-pointer">
+      <NavIcon
+        className={`w-5 h-5 ${isActive ? 'text-green-focus' : 'text-grey'}`}
+      />
+      <span className="font-medium text-sm"> {text}</span>
+    </div>
+  );
+}
