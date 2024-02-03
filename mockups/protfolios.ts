6ed1745b8a5601980f39cf5f2338e0f83cd3a7fd
Hizkias Abraham <hizkiasnew@gmail.com>
2024-02-03 15:42:18 +0300

    add portifolio mockup

---
+++ mockups/protfolios.ts
@@ -0,0 +1,37 @@
+export const portifiolios = [
+  {
+    name: 'Portifilio 1',
+    sizeKwdc: 2100,
+    numberOfProjects: 2,
+    revenue: 890228,
+    ar: 80332,
+  },
+  {
+    name: 'Portifilio 2',
+    sizeKwdc: 5250,
+    numberOfProjects: 7,
+    revenue: 1340089,
+    ar: 865159,
+  },
+  {
+    name: 'Portifilio 3',
+    sizeKwdc: 3500,
+    numberOfProjects: 4,
+    revenue: 760745,
+    ar: 340309,
+  },
+  {
+    name: 'Portifilio 4',
+    sizeKwdc: 2900,
+    numberOfProjects: 5,
+    revenue: 902202,
+    ar: 210068,
+  },
+  {
+    name: 'Portifilio 5',
+    sizeKwdc: 2960,
+    numberOfProjects: 1,
+    revenue: 976042,
+    ar: 16042,
+  },
+];
