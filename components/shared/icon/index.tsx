443b8a935eff556ea91717871f86c0ab6af9571f
Hizkias Abraham <hizkiasnew@gmail.com>
2024-02-03 10:13:38 +0300

    add arrow up right and down left icon

--- components/shared/icon/index.tsx
+++ components/shared/icon/index.tsx
@@ -6,6 +6,20 @@ import ArrowRightIcon from '@heroicons/react/24/outline/ArrowRightIcon';
 import UploadIcon from '@heroicons/react/24/outline/ArrowUpTrayIcon';
 import SearchIcon from '@heroicons/react/24/outline/MagnifyingGlassIcon';
 import CalendarIcon from '@heroicons/react/24/outline/CalendarIcon';
+import ArrowURight from '@heroicons/react/24/outline/ArrowUpRightIcon';
+import ArrowDLeft from '@heroicons/react/24/outline/ArrowDownLeftIcon';
+
+const ArrowUpRight = () => (
+  <div className="w-4 h-4 rounded-full inline-flex items-center justify-center bg-green text-white text-xl font-bold">
+    <ArrowURight className="w-3 h-3" />
+  </div>
+);
+
+const ArrowDownLeft = () => (
+  <div className="w-4 h-4 rounded-full inline-flex items-center justify-center bg-red text-white text-xl font-bold">
+    <ArrowDLeft className="w-3 h-3" />
+  </div>
+);
 
 export const Icon = {
   Home: (props: any) => <HomeIcon {...props} />,
@@ -16,4 +30,6 @@ export const Icon = {
   Upload: (props: any) => <UploadIcon {...props} />,
   Search: (props: any) => <SearchIcon {...props} />,
   Calendar: (props: any) => <CalendarIcon {...props} />,
+  ArrowUpRight,
+  ArrowDownLeft,
 };
