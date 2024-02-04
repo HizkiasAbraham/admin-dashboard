857658497084fdce359dfe4fd075771ac422e62f
Hizkias Abraham <hizkiasnew@gmail.com>
2024-02-04 10:18:49 +0300

    add bank credit mockup

---
+++ mockups/bank-credit.ts
@@ -0,0 +1,14 @@
+export const bankedCredits = {
+  hostBankCredits: [
+    { label: 'Previous Bank', value: 485 },
+    { label: 'New Bank Additions', value: 0 },
+    { label: 'New Bank Release', value: 485 },
+    { currentBank: 'Current Bank', value: 0 },
+  ],
+  customerBankCredits: [
+    { label: 'Previous Bank', value: 924 },
+    { label: 'New Bank Additions', value: 24 },
+    { label: 'New Bank Release', value: 266 },
+    { currentBank: 'Current Bank', value: 682 },
+  ],
+};
