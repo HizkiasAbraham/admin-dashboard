71875b147d3f1db59a11a4abac424e5df94c1f62
Hizkias Abraham <hizkiasnew@gmail.com>
2024-02-04 12:45:43 +0300

    add customers mock data

---
+++ mockups/customers.ts
@@ -0,0 +1,29 @@
+export const customers = [
+  {
+    accountNumber: '#334509875',
+    customerType: 'Residential',
+    serviceClass: 1,
+    discount: 5,
+    customerBank: 6655.55,
+    historic12MonthKwh: 12342,
+    allocationOfProject: 0.05,
+  },
+  {
+    accountNumber: '#703244240',
+    customerType: 'Small Commercial',
+    serviceClass: 1,
+    discount: 5,
+    customerBank: 6265.11,
+    historic12MonthKwh: 12342,
+    allocationOfProject: 0.05,
+  },
+  {
+    accountNumber: '#0943788326',
+    customerType: 'Large Commercial',
+    serviceClass: 1,
+    discount: 5,
+    customerBank: 5063.9,
+    historic12MonthKwh: 12342,
+    allocationOfProject: 0.05,
+  },
+];
