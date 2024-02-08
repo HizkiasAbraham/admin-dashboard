38da76c6c0788fcf72c9d47842765d86b5e8cd17
Hizkias Abraham <hizkiasnew@gmail.com>
2024-02-08 13:07:30 +0300

    add solid button type

---
+++ components/shared/buttons/solid-button/index.tsx
@@ -0,0 +1,13 @@
+import { SolidButtonInput } from './types';
+
+export function SolidButton(props: SolidButtonInput) {
+  const { bg, children, onClick } = props;
+  return (
+    <button
+      onClick={onClick}
+      className={`bg-${bg || 'white-smoke'} rounded-xl min-w-16`}
+    >
+      <div className="p-2 text-black font-medium text-sm">{children}</div>
+    </button>
+  );
+}
---
+++ components/shared/buttons/solid-button/types.ts
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react';
+
+export type SolidButtonInput = {
+  bg?: string;
+  children?: ReactNode;
+  onClick?: any;
+};
