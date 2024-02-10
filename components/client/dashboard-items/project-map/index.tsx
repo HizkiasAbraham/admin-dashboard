337bc9e487dff695b5d299e39783d82d99134623
Hizkias Abraham <hizkiasnew@gmail.com>
2024-02-10 13:04:56 +0300

    add project map dashboard item

---
+++ components/client/dashboard-items/project-map/index.tsx
@@ -0,0 +1,12 @@
+import { Card, CardContent, CardHeading } from '@/components/shared/card';
+
+export function ProjectMap() {
+  return (
+    <Card>
+      <div className="mt-3">
+        <CardHeading title="Project Map" />
+      </div>
+      <CardContent>Here goes card map content</CardContent>
+    </Card>
+  );
+}
