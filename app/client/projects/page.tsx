deecbd2959710364edf98e3eb49b1d949fca2086
Hizkias Abraham <hizkiasnew@gmail.com>
2024-02-10 14:06:43 +0300

    add projects page

--- app/client/projects/page.tsx
+++ app/client/projects/page.tsx
@@ -1,3 +1,10 @@
+import { ProjectsTable } from '@/components/client/tables/projects-table';
+import { projects } from '@/mockups/projects';
+
 export default function Projets() {
-  return <div>Here goes the projects list</div>;
+  return (
+    <div className="mt-3 w-full overflow-x-scroll md:overflow-x-hidden">
+      <ProjectsTable data={[...projects, ...projects] as []} />
+    </div>
+  );
 }
