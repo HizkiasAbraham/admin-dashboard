1ed5a7070bb04d073028a1b8f6a33e3413bdb900
Hizkias Abraham <hizkiasnew@gmail.com>
2024-03-02 16:38:36 +0300

    add invoices mockup

---
+++ mockups/invoices.ts
@@ -0,0 +1,34 @@
+export const invoicesData = [
+  {
+    status: "Closed",
+    date: "1/15/2020",
+    balance: 0,
+    appliedCredits: 142.2,
+    yourBill: 127.98,
+    savings: 14.22,
+  },
+  {
+    status: "Closed",
+    date: "1/15/2020",
+    balance: 0,
+    appliedCredits: 142.2,
+    yourBill: 127.98,
+    savings: 14.22,
+  },
+  {
+    status: "Closed",
+    date: "1/15/2020",
+    balance: 0,
+    appliedCredits: 142.2,
+    yourBill: 127.98,
+    savings: 14.22,
+  },
+  {
+    status: "Closed",
+    date: "1/15/2020",
+    balance: 0,
+    appliedCredits: 142.2,
+    yourBill: 127.98,
+    savings: 14.22,
+  },
+];
