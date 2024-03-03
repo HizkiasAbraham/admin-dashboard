3d144051e36b528058e6bbb91711760a63464c77
Hizkias Abraham <hizkiasnew@gmail.com>
2024-03-03 08:20:56 +0300

    add faq mock data

---
+++ mockups/faq.ts
@@ -0,0 +1,39 @@
+const content =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque eveniet, nisi iusto velit ea ipsum magnam illo ipsa placeat cum perferendis mollitia, voluptate ducimus, ut quas eius. Sunt, aut suscipit!";
+
+export const faqs = [
+  {
+    title: "What is community solar?",
+    content,
+  },
+  {
+    title:
+      "Does participating in a community solar program change my energy supplier or how I receive power?",
+    content,
+  },
+  {
+    title: "How do I save with community choice solar?",
+    content,
+  },
+  {
+    title: `How does community choice solar work?`,
+    content,
+  },
+  {
+    title: `How is my discount guaranteed?`,
+    content,
+  },
+  {
+    title: `Do I earn the same number of credits
+    every month?`,
+    content,
+  },
+  {
+    title: `What is a solar credit?`,
+    content,
+  },
+  {
+    title: `How will I be billed?`,
+    content,
+  },
+];
