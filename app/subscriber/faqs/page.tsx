e5d728b0d539682c04c8e834bb927e5ccd730821
Hizkias Abraham <hizkiasnew@gmail.com>
2024-03-03 08:24:07 +0300

    completed faq page

--- app/subscriber/faqs/page.tsx
+++ app/subscriber/faqs/page.tsx
@@ -1,9 +1,19 @@
+import { Accordion } from "@/components/shared/accordion/indext";
+import { Card, CardContent } from "@/components/shared/card";
 import { BreadCrumb } from "@/components/subscriber/bread-crumb";
+import { faqs } from "@/mockups/faq";
 
 export default function Faqs() {
   return (
     <div>
       <BreadCrumb pageName="Frequently Asked Questions" showSearchAndUpload />
+      <Card>
+        <CardContent>
+          {faqs.map((mock, i) => (
+            <Accordion key={i} title={mock.title} content={mock.content} />
+          ))}
+        </CardContent>
+      </Card>
     </div>
   );
 }
