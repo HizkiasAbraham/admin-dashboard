05cfda49ee4277446cb6de906fa8c83ece16f1ba
Hizkias Abraham <hizkiasnew@gmail.com>
2024-03-09 15:31:12 +0300

    integrate signin action with backend api

--- app/auth/login/actions.ts
+++ app/auth/login/actions.ts
@@ -1,8 +1,20 @@
 "use server";
 
+import { signIn } from "@/controllers/auth/signin";
+import { redirect } from "next/navigation";
+
 export async function login(_currentState: any, formData: FormData) {
-  const username = formData.get("username");
-  const password = formData.get("password");
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  return "Invalid username and password";
+  const username = formData.get("username") || "";
+  const password = formData.get("password") || "";
+  if (!username && !password) return "Please provide username and password";
+
+  try {
+    const result = await signIn(username as string, password as string);
+    if(result.status === 401) return 'Invalid username or password'
+    console.log('result', result.data)
+    redirect('/subscriber/dashboard')
+  } catch (error) {
+    console.log(error)
+    return "Something went wrong..."
+  }
 }
