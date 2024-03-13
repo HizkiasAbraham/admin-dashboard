7fc24d47d98f62b92892a88bf467f30527f0a7b3
Hizkias Abraham <hizkiasnew@gmail.com>
2024-03-13 13:43:51 +0300

    move logic inside controller files to route handling api endpoints

--- app/api/getUserInfo/route.ts
+++ app/api/getUserInfo/route.ts
@@ -1,4 +1,3 @@
-import { getUserInfo } from "@/controllers/auth/getUserInfo";
 import { cookies } from "next/headers";
 
 export async function GET(): Promise<Response> {
@@ -6,7 +5,18 @@ export async function GET(): Promise<Response> {
     const authToken = cookies().get("authToken");
     if (!authToken?.value)
       return Response.json({ message: "unautorized" }, { status: 401 });
-    const data = await getUserInfo(authToken.value);
+
+    const result = await fetch(
+      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/auth/userInfo`,
+      {
+        method: "GET",
+        headers: {
+          authorization: authToken.value,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await result.json();
     return Response.json({ data }, { status: 200 });
   } catch (error) {
     return Response.json({ message: "Something went wrong" }, { status: 500 });
