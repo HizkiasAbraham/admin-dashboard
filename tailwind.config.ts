64df664f19e21e3ae2b2d66ae5aead09fb9e2fb2
Hizkias Abraham <hizkiasnew@gmail.com>
2024-04-04 10:09:54 +0300

    add indeterminate loading component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,19 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        progress: "progress 1s infinite linear",
+      },
+      keyframes: {
+        progress: {
+          "0%": { transform: " translateX(0) scaleX(0)" },
+          "40%": { transform: "translateX(0) scaleX(0.4)" },
+          "100%": { transform: "translateX(100%) scaleX(0.5)" },
+        },
+      },
+      transformOrigin: {
+        "left-right": "0% 50%",
+      },
     },
     colors: {
       green: "#349946",
