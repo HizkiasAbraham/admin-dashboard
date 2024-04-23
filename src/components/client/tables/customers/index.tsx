e769e12f20e1778f23f1a731739a8222dd805368
Hizkias Abraham <hizkiasnew@gmail.com>
2024-04-23 14:26:37 +0300

    added table state and pagination to customers table

--- src/components/client/tables/customers/index.tsx
+++ src/components/client/tables/customers/index.tsx
@@ -3,12 +3,60 @@ import { Card, CardContent } from "@/src/components/shared/card";
 import { SearchInput } from "@/src/components/shared/inputs/searchInput";
 import { Paginator } from "@/src/components/shared/paginator";
 import { usd } from "@/src/utils/format-numbers";
-import { CustomerRow, CustomersInput } from "./types";
-import { useState } from "react";
+import { CustomersInput, TableProps } from "./types";
+import { useMemo, useState } from "react";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { Customer } from "../../types";
 
 export function CustomersTable(props: CustomersInput) {
   const { data: initialData } = props;
-  const [data, setData] = useState(initialData);
+  const [data, _setData] = useState(initialData);
+
+  const columns = useMemo<ColumnDef<Customer>[]>(
+    () => [
+      {
+        accessorKey: "accountNumber",
+        header: () => "Account Number",
+        cell: (info) => `#${info.getValue()}`,
+      },
+      {
+        accessorKey: "customerType",
+        header: () => "Customer Type",
+      },
+      {
+        accessorKey: "serviceClass",
+        header: () => "Service Class",
+      },
+      {
+        accessorKey: "discount",
+        header: () => "Discount %",
+        cell: (info) => `${info.getValue()}%`,
+      },
+      {
+        accessorKey: "customerBank",
+        header: () => "Customer Bank",
+        cell: (info) => `${usd(2).format((info.getValue() as number) || 0)}`,
+      },
+      {
+        accessorKey: "historical12MonthkWh",
+        header: () => "Historical 12 Month kWh",
+        cell: (info) =>
+          `${(info.getValue() as number).toLocaleString("en-US")}`,
+      },
+      {
+        accessorKey: "allocationOfProject",
+        header: () => "Allocation % of Project",
+        cell: (info) => `${(info.getValue() as number).toFixed(2)}%`,
+      },
+    ],
+    []
+  );
 
   return (
     <Card>
@@ -22,87 +70,82 @@ export function CustomersTable(props: CustomersInput) {
             <OutlinedButton color="green">Export .csv</OutlinedButton>
           </div>
         </div>
-        <div className="mt-4">
-          <TableHeading />
-          {data?.map((row, index) => (
-            <TableRow key={index} row={row} />
-          ))}
-          <div className="mt-4">
-            <Paginator alignment="end" />
-          </div>{" "}
-        </div>
+        <Table columns={columns} data={data || []} />
       </CardContent>
     </Card>
   );
 }
 
-function TableHeading() {
-  return (
-    <div className="flex w-full gap-1">
-      <div className="w-full flex justify-start p-2 pl-4 pb-0">
-        <p className="text-xs text-grey">Account Number </p>
-      </div>
-      <div className="w-full flex justify-start p-2 pb-0">
-        <p className="text-xs text-grey pl-4"> Customer Type</p>
-      </div>
-      <div className="w-full flex justify-end p-2 pr-4 pb-0">
-        <p className="text-xs text-grey">Service Class</p>
-      </div>
-      <div className="w-full flex justify-end p-2 pr-4 pb-0">
-        <p className="text-xs text-grey">Discount %</p>
-      </div>
-      <div className="w-full flex justify-end p-2 pr-4 pb-0">
-        <p className="text-xs text-grey">Customer Bank</p>
-      </div>
-      <div className="w-full flex justify-end p-2 pr-4 pb-0">
-        <p className="text-xs text-grey">Historical 12 Month kWh</p>
-      </div>
-      <div className="w-full flex justify-end p-2 pr-4 pb-0">
-        <p className="text-xs text-grey">Allocation % of Project</p>
-      </div>
-    </div>
-  );
-}
+function Table({ data, columns }: TableProps) {
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 5,
+  });
 
-function TableRow(props: CustomerRow) {
-  const { row } = props;
+  const table = useReactTable({
+    columns,
+    data,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onPaginationChange: setPagination,
+    state: {
+      pagination,
+    },
+  });
+
   return (
-    <div className="rounded-xl bg-white-smoke flex mt-2 mb-3 gap-2 cursor-pointer">
-      <div className="w-full flex p-4 pl-4">
-        <div className="w-full flex justify-start items-center border-r-2 border-inactive">
-          <p className="font-bold text-black text-sm">#{row?.accountNumber}</p>
-        </div>
-        <div className="w-full flex justify-start items-center border-r-2 border-inactive">
-          <p className="font-bold text-black text-sm pl-4">
-            {row?.customerType}
-          </p>
-        </div>
-        <div className="w-full flex justify-end items-center border-r-2 border-inactive">
-          <p className="font-medium text-black text-sm pr-4">
-            {row?.serviceClass}
-          </p>
-        </div>
-        <div className="w-full flex justify-end items-center border-r-2 border-inactive">
-          <p className="font-medium text-black text-sm pr-4">
-            {row?.discount}%
-          </p>
-        </div>
-        <div className="w-full flex justify-end items-center border-r-2 border-inactive">
-          <p className="font-medium text-black text-sm pr-4">
-            {usd(2).format(row?.customerBank || 0)}
-          </p>
-        </div>
-        <div className="w-full flex justify-end items-center border-r-2 border-inactive">
-          <p className="font-medium text-black text-sm pr-4">
-            {row?.historical12MonthkWh?.toLocaleString("en-US")}
-          </p>
-        </div>
-        <div className="w-full flex justify-end items-center pr-2">
-          <p className="font-bold text-black text-sm">
-            {row?.allocationOfProject?.toFixed(2)}%
-          </p>
-        </div>
+    <>
+      <div className="mt-4">
+        {table.getHeaderGroups().map((headerGroup) => (
+          <div key={headerGroup.id} className="flex w-full gap-1">
+            {headerGroup.headers.map((header, i) => (
+              <div
+                key={header.id}
+                className={`w-full flex justify-start ${!i ? "pl-4" : ""}`}
+              >
+                <p className="text-xs text-grey">
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                </p>
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
-    </div>
+      <div className="mt-4">
+        {table.getRowModel().rows.map((row) => {
+          return (
+            <div
+              key={row.id}
+              className="rounded-xl bg-white-smoke flex mt-2 mb-3 gap-2 cursor-pointer"
+            >
+              {row.getVisibleCells().map((cell, i) => {
+                return (
+                  <div key={cell.id} className="w-full flex p-4 pl-4">
+                    <div
+                      className={`w-full flex justify-start items-center ${
+                        i !== row.getAllCells().length - 1 ? "border-r-2" : ""
+                      } border-inactive`}
+                    >
+                      <p className="font-bold text-black text-sm">
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+      <div className="mt-4">
+        <Paginator alignment="end" table={table} />
+      </div>
+    </>
   );
 }
