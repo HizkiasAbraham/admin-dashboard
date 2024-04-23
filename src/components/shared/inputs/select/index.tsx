b167950ac3c65040e11cf1e0a4a3b804d90e9218
Hizkias Abraham <hizkiasnew@gmail.com>
2024-04-23 14:22:22 +0300

    updated select component

--- src/components/shared/inputs/select/index.tsx
+++ src/components/shared/inputs/select/index.tsx
@@ -1,18 +1,30 @@
-import { SelectInput } from './types';
+import { SelectInput } from "./types";
 
 export function Select(props: SelectInput) {
-  const { placeHolder, width, options } = props;
+  const { placeHolder, value, width, options, onChange } = props;
 
   return (
-    <select className={`border border-inactive rounded-xl ${width || ''}`}>
-      <option disabled value="" className="text-grey">
-        {placeHolder}
-      </option>
-      {options.map((opt, index) => (
-        <option key={index} value={opt.value}>
-          {opt.label}
+    <select
+      value={value}
+      className={`border border-inactive rounded-xl ${width || ""}`}
+      onChange={(e) => onChange(e.target.value)}
+    >
+      {placeHolder && (
+        <option disabled value="" className="text-grey">
+          {placeHolder}
         </option>
-      ))}
+      )}
+      {options.map((opt, index) =>
+        typeof opt === "object" ? (
+          <option key={index} value={opt.value}>
+            {opt.label}
+          </option>
+        ) : (
+          <option key={index} value={opt}>
+            {opt}
+          </option>
+        )
+      )}
     </select>
   );
 }
