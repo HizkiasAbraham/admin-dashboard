0f92fcc8ee9641ce647a9621e363db5a8332a40e
Hizkias Abraham <hizkiasnew@gmail.com>
2024-04-23 14:25:00 +0300

    updated paginator component integrating with tanstack

--- src/components/shared/paginator/index.tsx
+++ src/components/shared/paginator/index.tsx
@@ -1,36 +1,73 @@
-import { OutlinedButton } from '../buttons/outlined-button';
-import { Icon } from '../icon';
-import { Select } from '../inputs/select';
-import { PaginatorInput } from './types';
+import { OutlinedButton } from "../buttons/outlined-button";
+import { Icon } from "../icon";
+import { Select } from "../inputs/select";
+import { PaginatorInput } from "./types";
 
 export function Paginator(props: PaginatorInput) {
-  const { alignment } = props;
+  const { alignment, table } = props;
   return (
-    <div className={`flex justify-${alignment || 'start'}`}>
+    <div className={`flex justify-${alignment || "start"}`}>
       <div className="flex gap-3 items-center">
-        <p className="font-medium text-sm text-grey">1 - 10 of 1000</p>
+        <p className="font-medium text-sm text-grey">
+          Page {table?.getState()?.pagination?.pageIndex + 1 || 0} of{" "}
+          {table?.getPageCount()?.toLocaleString()}
+        </p>
         <p className="font-medium text-sm text-grey">Show:</p>
-        <Select placeHolder="10" width="w-24" options={[]} />
-        <OutlinedButton color="inactive">
-          <div className="flex items-center">
-            <p className="text-xs text-grey">|</p>{' '}
-            <Icon.ChevronLeft className="text-grey w-5 h-5" />
+        <Select
+          width="w-24"
+          options={[5, 10, 15, 20]}
+          onChange={(value: number) => {
+            table?.setPageSize(value);
+          }}
+        />
+        <OutlinedButton
+          color={table?.getCanPreviousPage() ? "green" : "inactive"}
+          onClick={() => table?.firstPage()}
+        >
+          <div
+            className={`flex items-center ${
+              table?.getCanPreviousPage() ? "text-green" : "text-grey"
+            }`}
+          >
+            <p className="text-xs ">|</p>{" "}
+            <Icon.ChevronLeft className="w-5 h-5" />
           </div>
         </OutlinedButton>
-        <OutlinedButton color="inactive">
-          <div className="flex items-center">
-            <Icon.ChevronLeft className="text-grey w-5 h-5" />
+        <OutlinedButton
+          color={table?.getCanPreviousPage() ? "green" : "inactive"}
+          onClick={() => table?.previousPage()}
+        >
+          <div
+            className={`flex items-center ${
+              table?.getCanPreviousPage() ? "text-green" : "text-grey"
+            }`}
+          >
+            <Icon.ChevronLeft className="w-5 h-5" />
           </div>
         </OutlinedButton>
-        <OutlinedButton color="inactive">
-          <div className="flex items-center">
-            <Icon.ChevronRight className="text-green w-5 h-5" />
+        <OutlinedButton
+          color={table?.getCanNextPage() ? "green" : "inactive"}
+          onClick={() => table?.nextPage()}
+        >
+          <div
+            className={`flex items-center ${
+              table?.getCanNextPage() ? "text-green" : "text-grey"
+            }`}
+          >
+            <Icon.ChevronRight className="w-5 h-5" />
           </div>
         </OutlinedButton>
-        <OutlinedButton color="inactive">
-          <div className="flex items-center">
-            <Icon.ChevronRight className="text-green w-5 h-5" />
-            <p className="text-xs text-green">|</p>{' '}
+        <OutlinedButton
+          color={table?.getCanNextPage() ? "green" : "inactive"}
+          onClick={() => table?.lastPage()}
+        >
+          <div
+            className={`flex items-center ${
+              table?.getCanNextPage() ? "text-green" : "text-grey"
+            }`}
+          >
+            <Icon.ChevronRight className="w-5 h-5" />
+            <p className="text-xs">|</p>{" "}
           </div>
         </OutlinedButton>
       </div>
